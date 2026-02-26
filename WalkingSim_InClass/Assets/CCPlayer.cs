076950459b5fde261acef1b790bd0a474453b344
LGuoL <2741629181@qq.com>
2026-02-26 13:54:03 +0800

    1

--- WalkingSim_InClass/Assets/CCPlayer.cs
+++ WalkingSim_InClass/Assets/CCPlayer.cs
@@ -1,89 +1,99 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
-public class Player : MonoBehaviour
+public class CCPlayer : MonoBehaviour
 {
-    [Header("Movement")]
     public float walkSpeed = 5;
     public float runSpeed = 9;
+
     public float jumpHeight = 5;
-
     public Transform cameraTransform;
-    public float lookSensativity = 1f;
+    public float lookSensitivity = 1f;
 
-    private CharacterController gg;
+    private CharacterController cc;
+
     private Vector2 moveInput;
     private Vector2 lookInput;
-    private float verticalVelocity;
-    private float gravity = -20f;
-    private float pitch;
+    private float verticalVelocity; //current upward/downward speed
+    private float gravity = -20f; //constant downard accleration
+    private float pitch; //up and down
 
-
+    //interaction variables
     private GameObject currentTarget;
     public Image reticleImage;
     private bool interactPressed;
 
-
     private bool isRunning;
     private bool isJumping;
 
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
-        gg = GetComponent<CharacterController>();
+        cc = GetComponent<CharacterController>();
 
+        //optional cursor locking
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
 
+        //find the reticle
         reticleImage = GameObject.Find("Reticle").GetComponent<Image>();
-        reticleImage.color = new Color(r: 0, g: 0, b: 0, a: .7f);
+        reticleImage.color = new Color(0, 0, 0, .7f); //slightly transparent black
     }
 
-
-    private void Update()
+    // Update is called once per frame
+    void Update()
     {
         HandleLook();
         HandleMovement();
         CheckInteract();
         HandleInteract();
     }
+
     private void HandleLook()
     {
-        float yaw = lookInput.x * lookSensativity;
-
-        float pitchDelta = lookInput.y * lookSensativity;
+        //horizontal mouse movement rotates player
+        float yaw = lookInput.x * lookSensitivity;
+        //vertical mouse movement rotates camera
+        float pitchDelta = lookInput.y * lookSensitivity;
 
         transform.Rotate(Vector3.up * yaw);
 
+        //accumulate vertical rotation
         pitch -= pitchDelta;
+        //clamp it so we dont flip upside down
         pitch = Mathf.Clamp(pitch, -90, 90);
 
         cameraTransform.localRotation = Quaternion.Euler(pitch, 0, 0);
     }
-
     private void HandleMovement()
     {
-        bool grounded = gg.isGrounded;
-        Debug.Log("is grounded: " +  grounded);
+        //updating our bool to be true or false if the player is grounded
+        bool grounded = cc.isGrounded;
+        Debug.Log("is grounded: " + grounded);
 
-        if (grounded && verticalVelocity < 0)
+        //this keeps the cc snapped to the ground
+        if (grounded && verticalVelocity <= 0)
         {
             verticalVelocity = -2f;
         }
 
         float currentSpeed = walkSpeed;
 
+        //if running is true set the current speed to run speed
         if (isRunning)
         {
             currentSpeed = runSpeed;
-        }
-        else if (!isRunning)
+        }//if it is false set it back to walk speed
+        else
         {
             currentSpeed = walkSpeed;
         }
 
         Vector3 move = transform.right * moveInput.x * currentSpeed + transform.forward * moveInput.y * currentSpeed;
 
+        //if jumping is true and we are grounded
         if (isJumping && grounded)
         {
             verticalVelocity = Mathf.Sqrt(jumpHeight * -2f * gravity);
@@ -93,14 +103,16 @@ public class Player : MonoBehaviour
             isJumping = false;
         }
 
+        //apply gravity to every frame
         verticalVelocity += gravity * Time.deltaTime;
 
+        //convert vertical velocity into movement vector
         Vector3 velocity = Vector3.up * verticalVelocity;
-
-        gg.Move((move + velocity) * Time.deltaTime);
-
+        //now WE ARE FINALLY MOVING OUR PLAYER
+        cc.Move((move + velocity) * Time.deltaTime);
 
     }
+
     void CheckInteract()
     {
         //reset reticle image to normal color first
@@ -141,7 +153,6 @@ public class Player : MonoBehaviour
 
     }
 
-
     public void OnMove(InputAction.CallbackContext context)
     {
         moveInput = context.ReadValue<Vector2>();
@@ -154,7 +165,8 @@ public class Player : MonoBehaviour
 
     public void OnJump(InputAction.CallbackContext context)
     {
-        if(context.performed) isJumping = true;
+        //if we are actually hitting the key is jumping equals true!
+        if (context.performed) isJumping = true;
     }
 
     public void OnSprint(InputAction.CallbackContext context)
@@ -164,11 +176,11 @@ public class Player : MonoBehaviour
 
     public void OnInteract(InputAction.CallbackContext context)
     {
-        if(context.performed) interactPressed = true; 
+        if (context.performed) interactPressed = true;
     }
 
     private void OnControllerColliderHit(ControllerColliderHit hit)
     {
-        Debug.Log("gg Collided with: " + hit.gameObject.name);
+        Debug.Log("CC Collided with: " + hit.gameObject.name);
     }
 }
