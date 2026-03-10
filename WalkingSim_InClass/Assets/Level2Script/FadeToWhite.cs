39725db4506e0d0d0c354b0d6f2b336a6b9937d3
LGuoL <2741629181@qq.com>
2026-03-10 17:10:59 +0800

    4

---
+++ WalkingSim_InClass/Assets/Level2Script/FadeToWhite.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class FadeToWhite : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
